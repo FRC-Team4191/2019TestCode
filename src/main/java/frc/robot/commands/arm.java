7e415b0974875da3ce09d2179c95c6b042e78619
Arman Özcan <arman.2002@windowslive.com>
2019-11-02 00:59:01 +0300

    yazdım bi şeler

--- src/main/java/frc/robot/commands/arm.java
+++ src/main/java/frc/robot/commands/arm.java
@@ -8,9 +8,15 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.subsystems.robotArm;
 
-public class OpenClaw extends Command {
-  public OpenClaw() {
+public class arm extends Command {
+  int kol;
+  public arm(int a) {
+    requires(Robot.robotArm);
+    kol=a;
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
@@ -23,6 +29,7 @@ public class OpenClaw extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+  Robot.robotArm.topfırlat(kol);
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -34,6 +41,7 @@ public class OpenClaw extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+  Robot.robotArm.topfırlat(0);
   }
 
   // Called when another command which requires one or more of the same
