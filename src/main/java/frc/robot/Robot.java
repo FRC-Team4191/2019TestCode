c3d12c378bc46f72b9b27d0cb987cd11b8d960b6
unknown <yokbarkn@gmail.com>
2019-11-10 22:31:21 +0300

    reset

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-//deneme2
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
